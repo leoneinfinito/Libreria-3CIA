24ad1291ce8dd7acb3f188a53a4ba629db9639fc
leoneinfinito <samuelepelella22@gmail.com>
2026-04-20 21:23:45 +0200

    Update 3cia.h

--- libreria/3cia.h
+++ libreria/3cia.h
@@ -1,23 +1,25 @@
-#ifndef PELELLA_H
-#define PELELLA_H
+#ifndef CIA_H
+#define CIA_H
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <math.h>
 
 //--------------------------Prezzo Scontato---------------------------------
 
 static float PrezzoScontato( float prezzo , int sconto ){
-    return prezzo - (prezzo * sconto / 100);
+    return prezzo - (prezzo * sconto / 100);      // calcolo del prezzo scontato
 }
 
-//--------------------------Leggi stringa----------------------------------- (non avevo voglia di memorizzare "scanf("%[^\n]",..)" )
+//--------------------------Leggi stringa-----------------------------------
 
 static void LeggiStringa(char frase[], int dimensione) {
     int i = 0;
-    fgets(frase, dimensione, stdin);
+    fgets(frase, dimensione, stdin);	// legge una stringa da input e la memorizza in frase
 
     while (frase[i] != '\0') {
-        if (frase[i] == '\n') {
+        if (frase[i] == '\n') {		
             frase[i] = '\0';
         }
         i++;
@@ -28,9 +30,9 @@ static void LeggiStringa(char frase[], int dimensione) {
 
 static int DifferenzaAssoluta (int a , int b ){
     if (a > b) {
-        return a - b;
+        return a - b;		// se a è maggiore di b, la differenza assoluta è a - b
     } else {
-        return b - a;
+        return b - a;		// altrimenti, la differenza assoluta è b - a
     }
 }
 
@@ -38,7 +40,7 @@ static int DifferenzaAssoluta (int a , int b ){
 static int SommaDa(int n) {
     int somma = 0;
     int i;
-    for (i = 1; i <= n; i++) {
+    for (i = 1; i <= n; i++) {	
         somma = somma + i;
     }
     return somma;
@@ -46,10 +48,10 @@ static int SommaDa(int n) {
 //---------------------------------Fattoriale-------------------------------------
 
 static int Fattoriale ( int n){
-    if (n == 0) {
-        return 1;
+    if (n == 0) {		// se n è 0, il fattoriale è 1
+        return 1; 	
     } else {
-        return n * Fattoriale(n - 1);
+        return n * Fattoriale(n - 1);	// altrimenti, il fattoriale di n è n moltiplicato per il fattoriale di n-1
     }
 }
 
@@ -62,23 +64,23 @@ static int Palindroma(char stringa[]) {
     int indiceFine;
 
     while (stringa[lunghezzaDellaStringa] != '\0') {
-        lunghezzaDellaStringa = lunghezzaDellaStringa + 1;
+        lunghezzaDellaStringa = lunghezzaDellaStringa + 1;			// calcolo della lunghezza della stringa
     }
 
-    indiceFine = lunghezzaDellaStringa - 1;
+    indiceFine = lunghezzaDellaStringa - 1;			// indiceFine è l'indice dell'ultimo carattere della stringa
 
     while (indiceInizio < indiceFine) {
 
         // salta spazi a sinistra
-        if (stringa[indiceInizio] == ' ') {
+        if (stringa[indiceInizio] == ' ') {		// se il carattere in posizione indiceInizio è uno spazio, incremento indiceInizio per saltare lo spazio
             indiceInizio = indiceInizio + 1;
         }
         // salta spazi a destra
-        else if (stringa[indiceFine] == ' ') {
+        else if (stringa[indiceFine] == ' ') { // se il carattere in posizione indiceFine è uno spazio, decremento indiceFine per saltare lo spazio
             indiceFine = indiceFine - 1;
         }
         else {
-            if (stringa[indiceInizio] != stringa[indiceFine]) {
+            if (stringa[indiceInizio] != stringa[indiceFine]) {  // se i caratteri in posizione indiceInizio e indiceFine sono diversi, la stringa non è palindroma
                 return 0;
             }
             indiceInizio = indiceInizio + 1;
@@ -89,7 +91,143 @@ static int Palindroma(char stringa[]) {
     return 1;
 }
 
-//--------------------------------------------------------------------------
+//----------------Media Dentro Un Vettore-------------------------
 
+static float media(int numeri[], int dimensione){
+    int somma = 0;
+    int i;
+
+    for(i = 0; i < dimensione; i++){
+        somma = somma + numeri[i];
+    }
+
+    return somma / dimensione; 
+}
+
+//-------------------------Conta Numeri Sopra La Media-----------------------------------------
+
+static int contaSopraMedia(int numeri[], int dimensione){
+    int i;
+    int conta = 0;
+    float Media;
+
+    Media = media(numeri, dimensione);
+
+    for(i = 0; i < dimensione; i++){
+        if(numeri[i] > Media){
+            conta++;
+        }
+    }
+
+    return conta;
+}
+
+//----------------Numero Massimo In Un Vettore-----------------
+
+static int massimo(int vettore[], int dimensione){
+    int i;
+    int max = vettore[0];
+
+    for(i = 1; i < dimensione; i++){
+        if(vettore[i] > max){
+            max = vettore[i];
+        }
+    }
+
+    return max;
+}
+
+//-------------------------Trasforma In Numero Assoluto------------------------------------
+
+static void TrasformaAssoluto(int num[], int dimensione){
+    int i;
+
+    for(i = 0; i < dimensione; i++){
+        num[i] = abs(num[i]);
+    }
+}
+
+//-----------------------Somma Riga Matrice--------------------------------
+
+static int sommaRiga(int rigaScelta, int colonne, int matrice[][colonne]){
+    int i;
+    int somma = 0;
+
+    for(i = 0; i < colonne; i++){
+        somma = somma + matrice[rigaScelta][i];
+    }
+
+    return somma;
+}
+
+//-------------------------Media Dentro Riga--------------------------------
+static float mediaRiga(int rigaScelta, int colonne, int matrice[][colonne]){
+    int somma;
+    float media;
+
+    somma = sommaRiga(rigaScelta, colonne, matrice);
+    
+    media = somma / colonne;
+    return media;
+}
+
+
+//-------------------------Numero Massimo Dentro Riga--------------------------------
+
+static int maxRiga(int rigaScelta, int colonne, int matrice[][colonne]){
+    int i;
+    int max = matrice[rigaScelta][0];
+
+    for(i = 1; i < colonne; i++){
+        if(matrice[rigaScelta][i] > max){
+            max = matrice[rigaScelta][i];
+        }
+    }
+
+    return max;
+}
+
+//----------------Verifica Pari o Dispari--------------------
+
+static int pari(int num){
+    return num % 2 == 0;
+}
+
+//-------------------Trasforma Elemento------------------------------
+
+static int trasformaElemento(int numero){
+    if(numero % 2 == 0){
+        return pow(numero, 2);  // se è pari -> quadrato
+    }else{
+    
+        return pow(numero, 3);  // se è dispari -> cubo
+    }
+}
+
+//-------------------Trasforma Matrice------------------------------
+
+static void trasformaMatrice(int righe, int colonne, int matrice[righe][colonne]){
+    int i;
+    int j;
+
+    for(i = 0; i < righe; i++){
+        for(j = 0; j < colonne; j++){
+            matrice[i][j] = trasformaElemento(matrice[i][j]);
+        }
+    }
+}
+
+//-------------------Somma Diagonale Matrice-------------------------------
+
+static int sommaDiagonale(int righe, int colonne, int matrice[righe][colonne]){
+    int i;
+    int somma = 0;
+
+    for(i = 0; i < righe; i++){
+        somma = somma + matrice[i][i];
+    }
+
+    return somma;
+}
 
 #endif
