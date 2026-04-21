6f981d9f45de91730d56b4bd55f166401e433c76
leoneinfinito <samuelepelella22@gmail.com>
2026-04-21 19:03:28 +0200

    Update sommada.c

--- file.c/sommada.c
+++ file.c/sommada.c
@@ -5,10 +5,10 @@ int main() {
  
     int numero;
 
-        printf("Inserisci un numero: ");
-        scanf("%d", &numero);
+    printf("Inserisci un numero: ");
+    scanf("%d", &numero);
     
-       printf("La somma da 1 a %d e' : %d\n", numero, SommaDa(numero));
+    printf("La somma da 1 a %d e' : %d\n", numero, SommaDa(numero));
 
     return 0;
 }
