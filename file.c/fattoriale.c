b60207f502f5142ee9dd603407b629db9665616d
leoneinfinito <samuelepelella22@gmail.com>
2026-04-21 19:03:52 +0200

    Update fattoriale.c

--- file.c/fattoriale.c
+++ file.c/fattoriale.c
@@ -5,10 +5,10 @@ int main() {
  
     int numero;
 
-        printf("Inserisci un numero: ");
-        scanf("%d", &numero);
+    printf("Inserisci un numero: ");
+    scanf("%d", &numero);
     
-       printf("Il fattoriale di %d e' : %d\n", numero, Fattoriale(numero));
+    printf("Il fattoriale di %d e' : %d\n", numero, Fattoriale(numero));
        
     return 0;
 }
